5a85bb4c2e792e555f669aebb58ad439f596ade6
Tom Bartlett <thomas.bartlett.1992@gmail.com>
2022-08-31 16:06:27 +0100

    Cleanup
    Signed-off-by: Tom Bartlett <thomas.bartlett.1992@gmail.com>

--- modules/stacks/remote_state/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  stack_config = merge(
-    var.stack_config,
-    {
-      # Inject default for unset optional vars here
-      # my_field = coalesce(var.stack_config.my_field, local.defaults.my_field)
-    }
-  )
-}

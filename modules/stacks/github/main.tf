90814c17b631735b041973cf304cb3f1b372d7ce
Tom Bartlett <thomas.bartlett.1992@gmail.com>
2022-09-08 16:44:43 +0100

    Cleanup
    Signed-off-by: Tom Bartlett <thomas.bartlett.1992@gmail.com>

--- modules/stacks/github/main.tf
+++ modules/stacks/github/main.tf
@@ -6,28 +6,3 @@ module "repos" {
   visibility = each.value.visibility 
   default_branch = each.value.default_branch 
 }
-
-#resource "github_repository" "repos" {
-#  for_each = var.stack_config.repositories
-#  name = each.key
-#  description = each.value.description
-#  visibility = each.value.visibility
-#}
-#
-#resource "github_branch" "branch" {
-#  for_each = var.stack_config.repositories
-#  repository = each.key
-#  branch = each.value.default_branch
-#  depends_on = [
-#    github_repository.repos 
-#  ]
-#}
-#
-#resource "github_branch_default" "branch_default" {
-#  for_each = var.stack_config.repositories
-#  repository = each.key
-#  branch = each.value.default_branch
-#  depends_on = [
-#    github_branch.branch
-#  ]
-#}

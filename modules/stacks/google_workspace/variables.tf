fc2b8bcc006215451dde92f94d6e3f581899e534
Tom Bartlett <31888238+bartlettt@users.noreply.github.com>
2022-09-09 16:16:31 +0100

    Added spf and dkim records (#6)
    Signed-off-by: Tom Bartlett <thomas.bartlett.1992@gmail.com>
    
    Signed-off-by: Tom Bartlett <thomas.bartlett.1992@gmail.com>

--- modules/stacks/google_workspace/variables.tf
+++ modules/stacks/google_workspace/variables.tf
@@ -8,6 +8,19 @@ variable "stack_config" {
       records = list(string)
       ttl     = optional(number, 300)
     })
+    spf_record = optional(object({
+      records = optional(list(string), ["v=spf1 include:_spf.google.com ~all"])
+      ttl     = optional(number, 3600)
+      }),
+      {
+        records = ["v=spf1 include:_spf.google.com ~all"]
+        ttl     = 3600
+    })
+    dkim_record = optional(object({
+      name    = optional(string, "google._domainkey")
+      records = list(string)
+      ttl     = optional(number, 300)
+    }))
   })
 }
 

be25d6a1ed69580992a3ea302f2462f4d1fdee71
Tom Bartlett <thomas.bartlett.1992@gmail.com>
2022-09-09 13:05:02 +0100

    Made sensible policy for github executor
    Signed-off-by: Tom Bartlett <thomas.bartlett.1992@gmail.com>

--- stacks/github/config.tf
+++ stacks/github/config.tf
@@ -1,19 +1,20 @@
 locals {
+  stack_name = basename(abspath(path.root))
   stack_config = {
-    name = basename(abspath(path.root))
+    name = local.stack_name
     repositories = {
       digital_cv = {}
       infrastructure = {
         terraform_executor_configs = {
           github = {
             aws_iam_user_config = {
-              policy_document = data.aws_iam_policy_document.test
+              policy_document = data.aws_iam_policy_document.github
             }
             github_pat_config = {}
           }
           remote_state = {
             aws_iam_user_config = {
-              policy_document = data.aws_iam_policy_document.test
+              policy_document = data.aws_iam_policy_document.remote_state
             }
           }
         }
--- stacks/github/data_iam_policies.tf
+++ stacks/github/data_iam_policies.tf
@@ -1,4 +1,16 @@
-data "aws_iam_policy_document" "test" {
+data "aws_iam_policy_document" "github" {
+  statement {
+    sid = "iam"
+    actions = [ "iam:*" ]
+    resources = [ 
+      "arn:aws:iam::*:user/${local.stack_name}-*",
+      "arn:aws:iam::*:policy/${local.stack_name}-*",
+    ]
+  }
+}
+
+
+data "aws_iam_policy_document" "remote_state" {
   statement {
     sid = "test"
     actions = [

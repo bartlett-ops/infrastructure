bb19ab57a467c35c804c48541424c66905236afb
Tom Bartlett <thomas.bartlett.1992@gmail.com>
2022-09-09 16:15:31 +0100

    Added spf and dkim records
    Signed-off-by: Tom Bartlett <thomas.bartlett.1992@gmail.com>

--- modules/stacks/google_workspace/main.tf
+++ modules/stacks/google_workspace/main.tf
@@ -5,3 +5,21 @@ resource "aws_route53_record" "mx" {
   records = var.stack_config.mx_record.records
   ttl     = var.stack_config.mx_record.ttl
 }
+
+resource "aws_route53_record" "spf" {
+  zone_id = var.stack_config.route53_zone.zone_id
+  name    = var.stack_config.route53_zone.name
+  type    = "TXT"
+  records = var.stack_config.spf_record.records
+  ttl     = var.stack_config.spf_record.ttl
+}
+
+resource "aws_route53_record" "dkim" {
+  count   = var.stack_config.dkim_record != null ? 1 : 0
+  zone_id = var.stack_config.route53_zone.zone_id
+  name    = var.stack_config.dkim_record.name
+  type    = "TXT"
+  records = var.stack_config.dkim_record.records
+  ttl     = var.stack_config.dkim_record.ttl
+}
+
--- stacks/google_workspace/config.tf
+++ stacks/google_workspace/config.tf
@@ -12,5 +12,8 @@ locals {
         "15 mtxvqsn7gptm7l2lcxkncc4jk5kw2uz7qfb3u56c33avtktna3fa.mx-verification.google.com.",
       ]
     }
+    dkim_record = {
+      records = ["v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkoUAYX6jb1/V7xue+em855JNGVucrNUE7ekLW89w/NDr3dUXj5C3S38Lb9xs7QC+/fM7AxV6XMx7BmSAydf5+eXLovNepYnTIbL8yJkp7hNHHzhBukRj6ZDYhCV6jQJ\"\"HqPjRS8T/BUNHj3uNiBuDPAylRLzEMlmUsk0VyIhz97nk1JaU0qv8RCPRPG+GvBjnl6wkZFboYGqI2yk2w+Um2uoCfMK5FG3f2XQZD6RXdSe6Evv21hrcCVcTLUCqsc1Ph9DXEC9/xHg5/tcklfRpJz1A7cXsM9KJnVAxc5Ft0wdgI5nCbGW+0PPfrgqq2UqjNgh0NK5ar8snI9hCMHfSXQIDAQAB"]
+    }
   }
 }

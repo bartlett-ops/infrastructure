37c2f855f3e8c0fb9ebda416762ef046926e37bd
Tom Bartlett <thomas.bartlett.1992@gmail.com>
2022-09-10 11:27:19 +0100

    Update executor permissions, and sensitive logic
    Signed-off-by: Tom Bartlett <thomas.bartlett.1992@gmail.com>

--- stacks/github/data_iam_policies.tf
+++ stacks/github/data_iam_policies.tf
@@ -7,6 +7,26 @@ data "aws_iam_policy_document" "github" {
       "arn:aws:iam::*:policy/${local.stack_name}-*",
     ]
   }
+  statement {
+    sid = "S3BackendBucket"
+    actions = [
+      "s3:ListBucket",
+    ]
+    resources = [
+      "arn:aws:s3:::bartlett-remote-states"
+    ]
+  }
+  statement {
+    sid = "S3BackendBucketObjects"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+    resources = [
+      "arn:aws:s3:::bartlett-remote-states/infrastructure/github/terraform.tfstate"
+    ]
+  }
 }
 
 

4f585f30d536b08ad5f52bb6de92ef00f80922f0
Tom Bartlett <tom@bartlett.sh>
2022-10-18 12:27:44 +0800

    Updated iam policy for remote_state stack executor
    Signed-off-by: Tom Bartlett <tom@bartlett.sh>

--- stacks/github/data_iam_policies.tf
+++ stacks/github/data_iam_policies.tf
@@ -43,12 +43,43 @@ data "aws_iam_policy_document" "github" {
 
 data "aws_iam_policy_document" "remote_state" {
   statement {
-    sid = "test"
+    sid = "S3"
     actions = [
-      "s3:ListAllMyBuckets"
+      "s3:*",
     ]
     resources = [
-      "*"
+      "arn:aws:s3:::bartlett-remote-states"
+    ]
+  }
+  statement {
+    sid = "KMS"
+    actions = [
+      "kms:*",
+    ]
+    resources = [
+      "arn:aws:kms:::terraform-state-lock"
+    ]
+  }
+  statement {
+    sid = "S3BackendBucketObjects"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+    resources = [
+      "arn:aws:s3:::bartlett-remote-states/infrastructure/remote_state/terraform.tfstate"
+    ]
+  }
+  statement {
+    sid = "S3BackendKMS"
+    actions = [
+      "dynamodb:PutItem",
+      "dynamodb:GetItem",
+      "dynamodb:DeleteItem",
+    ]
+    resources = [
+      "arn:aws:dynamodb:eu-west-1:150539654980:table/terraform-state-lock"
     ]
   }
 }
